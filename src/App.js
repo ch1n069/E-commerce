e2589b0cdb2d10aa929d2130eb31d0dbfe1e76de
Bruno Alpachino <brunolalpachino@gmail.com>
2022-11-04 21:43:52 +0300

    imported the home component into the App.js file

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
+import Home from "./pages/Home";
 function App() {
   return (
     <div>
       <Header />
       <main className="py-3">
-        <Container>Welcome</Container>
+        <Container>
+          Welcome
+          <Home />
+        </Container>
       </main>
       <Footer />
     </div>
