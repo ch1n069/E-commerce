cb785e28ceddc4902dc332b83a65467343d7f86b
Bruno Alpachino <brunolalpachino@gmail.com>
2022-11-07 14:04:27 +0300

    added react-router dom

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
           Welcome
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
           <Home />
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
