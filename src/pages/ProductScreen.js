f1bb661fab6ceff7c9f72799e0d1dd12ff5e661b
Bruno Alpachino <brunolalpachino@gmail.com>
2022-11-07 15:23:25 +0300

    added filter logic to display only one selected item

--- src/pages/ProductScreen.js
+++ src/pages/ProductScreen.js
@@ -1,7 +1,17 @@
 import React from "react";
+// import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
+import products from "../Products";
+import { Link, useParams } from "react-router-dom";
 
-const ProductScreen = () => {
-  return <div>product page will</div>;
+// imports happen above
+const ProductScreen = (props) => {
+  const params = useParams();
+  const product = products.find((p) => p._id === params.id);
+  return (
+    <div>
+      <h2>{product.name}</h2>
+    </div>
+  );
 };
 
 export default ProductScreen;
