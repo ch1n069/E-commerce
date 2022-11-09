f49d58a66049ccfe8e57d61143583518a051522c
Bruno Alpachino <brunolalpachino@gmail.com>
2022-11-09 09:15:10 +0300

    used the redux actions in the home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,29 +2,32 @@ import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Product from "../components/Product.js";
 import products from "../Products.js";
-import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/ProductActions";
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
-
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { error, loading, products } = productList;
   useEffect(() => {
-    async function fetchData() {
-      const { data } = await axios.get("/api/products/");
-      setProducts(data);
-    }
-    fetchData();
-  }, []);
-
+    dispatch(listProducts());
+  }, [dispatch]);
   return (
     <div>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>loading....</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </div>
   );
 };
