8377c77a2b4166a19fcb9d87c3f25c012f004b4e
Bruno Alpachino <brunolalpachino@gmail.com>
2022-11-19 00:15:50 +0300

    create actions to register user

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -2,9 +2,13 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
   USER_LOGOUT,
 } from "../constants/userConstants";
 import axios from "axios";
+
 export const Login = (email, password) => async (dispatch) => {
   try {
     dispatch({ type: USER_LOGIN_REQUEST });
@@ -38,4 +42,34 @@ export const Logout = () => (dispatch) => {
   dispatch({
     type: USER_LOGOUT,
   });
+};
+
+// USER REGISTER REDUCER ////////////////////////////////////////////////////////////
+
+export const userRegister = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: USER_REGISTER_REQUEST });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const { data } = await axios.post(
+      "/api/users/register/",
+      { name: name, username: email, password: password },
+      config
+    );
+
+    dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
 };
