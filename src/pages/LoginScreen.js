d05513801e69dda626ee7fbff97defefcf4b0ca6
Bruno Alpachino <brunolalpachino@gmail.com>
2022-11-18 21:01:41 +0300

    created login form

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -6,14 +6,51 @@ import Loader from "../components/Loader.js";
 import Message from "../components/Message.js";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../actions/userActions";
+import FormContainer from "../components/FormContainer.js";
 
 // imports happen above
 const LoginScreen = () => {
   // to grab form inputs
-  const [email, setEmail] = useState("  ");
-  const [password, setPassword] = useState("  ");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  return <div>LoginScreen</div>;
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
+
+  return (
+    <FormContainer>
+      <h1>Sign in to your account</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter your email address"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter your Password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          ></Form.Control>
+        </Form.Group>
+        <Button className="mt-3" type="submit" variant="primary">
+          Sign in
+        </Button>
+      </Form>
+    </FormContainer>
+  );
 };
 
 export default LoginScreen;
