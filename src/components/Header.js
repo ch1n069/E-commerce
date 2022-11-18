4af81e3c6aab2b87b556e895bd4cdba9d9b81db9
Bruno Alpachino <brunolalpachino@gmail.com>
2022-11-18 23:11:11 +0300

    add the logout action to the navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./Header.css";
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { dispatch, useSelector, useDispatch } from "react-redux";
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { Logout } from "../actions/userActions";
 const Header = () => {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(Logout());
+  };
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -21,11 +30,22 @@ const Header = () => {
                   <i class="bx bx-cart-alt cart"></i>Cart
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/login">
-                <Nav.Link>
-                  <i class="bx bx-log-in"></i>Login
-                </Nav.Link>
-              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <i class="bx bx-log-in"></i>Login
+                  </Nav.Link>
+                </LinkContainer>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
