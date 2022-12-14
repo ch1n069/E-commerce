f84f1357e70c1fbc1cf052e210df69fad3074eda
Bruno Alpachino <brunolalpachino@gmail.com>
2022-12-14 08:46:03 +0300

    created the user update from logic

--- src/pages/ProfileScreen.js
+++ src/pages/ProfileScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { LinkuseSelector, useLocation, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Row, Col, Button, Form } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../components/Loader";
@@ -10,14 +11,110 @@ import { getUserDetails } from "../actions/userActions";
 const ProfileScreen = () => {
   // variable to handle the routing logic
   const location = useLocation();
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
 
-  const userDetails = useSelector((state) => state.UserDetails);
+  //   the use state below is populate our form with data
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const userDetails = useSelector((state) => state.userDetails);
+
+  // //   destructuring of the user information
+  const { error, loading, user } = userDetails;
+
+  const userLogin = useSelector((state) => state.userLogin);
+
+  const { userInfo } = userLogin;
+
+  useEffect(() => {
+    if (!userInfo) {
+      navigate("/login");
+    } else {
+      if (!user || !user.name) {
+        dispatch(getUserDetails("profile"));
+      } else {
+        setName(user.name);
+        setEmail(user.email);
+      }
+    }
+  }, [dispatch, userInfo, user]);
+
+  // useEffect(() => {
+  //   if (!userInfo) {
+  //     navigate("/login");
+  //   } else {
+  //     // if there is no user information dispatch action
+  //     if (!user || user.name) {
+  //       dispatch(getUserDetails("profile"));
+  //     } else {
+  //     }
+  //   }
+  // }, [dispatch, userInfo, user]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("updating profile");
+  };
   return (
     <Row>
       <Col md={3}>
         <h2>User profile</h2>
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="email">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              required
+              type="name"
+              placeholder="Enter your name"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="email">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              required
+              type="email"
+              placeholder="Enter your email address"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="confirmPassword">
+            <Form.Label> Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Re-enter your password"
+              value={confirmPassword}
+              onChange={(e) => {
+                setConfirmPassword(e.target.value);
+              }}
+            ></Form.Control>
+          </Form.Group>
+          <Button className="mt-3" type="submit" variant="primary">
+            Update
+          </Button>
+        </Form>
       </Col>
       <Col md={9}>My orders</Col>
     </Row>
